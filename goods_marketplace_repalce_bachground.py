e985c49c3eb803d89e9e50d317dee979c059e4d1
Alyona Pashnina <alyonapashnina@MacBook-Air-Alyona.local>
2024-09-30 03:03:58 +0300

    done

--- goods_marketplace_repalce_bachground.py
+++ goods_marketplace_repalce_bachground.py
@@ -1,21 +1,47 @@
 # from PIL import Image
-import numpy as np
+# import numpy as np
 
-import cv2
-from rembg import remove
+# import cv2
+# from rembg import remove
+
+СOLOR_RGB = {'red': (255, 0, 0),
+             'pink': (255, 105, 180),
+             'orange': (255, 165, 0),
+             'yellow': (255, 255, 0),
+             'green': (0, 128, 0),
+             'cyan': (0, 255, 255),
+             'blue': (0, 0, 255),
+             'purple': (128, 0, 128),
+             'brown': (165, 42, 42),
+             'gray': (128, 128, 128),
+             'black': (0, 0, 0),
+             'white': (255, 255, 255),
+             'tan': (210, 180, 140),
+             'beige': (245, 245, 220),
+             'ivory': (255, 255, 240),
+             'thistle': (216, 191, 222)}
 
 
-def draw_new_back(image_filename, background_color=None, background_img_filename=None, save_img_new=None):
-  image = cv2.imread(image_filename)
-  image = remove(image)
+
+def draw_new_back(image_filename: str, background_color:tuple = None, background_img_filename:str = None):
+  """Меняет фон на карточке товара. Новая картинка сохраняется в эту же директорию.
+
+  Args:
+    image_filename (str): путь до файла с изначальным изображением.
+    background_color (tuple, optional): цвет для нового фона.
+    background_img_filename (str): путь до файла с изображением для нового фона.
+  """
+  image = cv2.imread(image_filename) # считываем файл
+  image = remove(image) # удаляем фон
   image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR) if image.shape[2] == 4 else image
 
-  mask = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+  mask = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY) # делаем маску для изображения
   _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
 
-  image_mask = cv2.bitwise_and(np.array(image), np.array(image), mask=mask)
+  image_mask = cv2.bitwise_and(np.array(image), np.array(image), mask=mask) # применяем маску 
   inverse_mask = cv2.bitwise_not(mask)
 
+  # подготавливаем новый фон (если фона не подали - возвращаем изображение без фона (или с прозрачным фоном))
   if background_img_filename:
     background_img = cv2.imread(background_img_filename)
     background_img = np.array(background_img)
@@ -30,11 +56,11 @@ def draw_new_back(image_filename, background_color=None, background_img_filename
     return image
 
   new_back = np.full(np.array(image).shape, backgr, dtype=np.uint8)
-  
-  background = cv2.bitwise_and(new_back, new_back, mask=inverse_mask)
+  background = cv2.bitwise_and(new_back, new_back, mask=inverse_mask) # наш фон
 
-  result = cv2.add(image_mask, background)
+  result = cv2.add(image_mask, background)  # скрещиваем фон и изображение
   
+   # сохранем новое изображение
   pred_img_res = Image.fromarray(result.astype(np.uint8))
   output_filename = f"{input_img.rstrip(input_img.split('/')[-1])}output_{input_img.split('/')[-1].split('.')[0]}.png"
   pred_img_res.save(output_filename, 'PNG')
@@ -45,9 +71,17 @@ def draw_new_back(image_filename, background_color=None, background_img_filename
   
 
 if __name__ == "__main__":
-  background_color = (210, 180, 170) # персиковый
-  background_img = '' # путь до файла с новым фоном
+  background_color = input().replace(' ','').replace('(','').replace(')','').split(',')
+  background_color = tuple(int(x) for x in background_color)
+  background_img = str(input())
+  input_img = str(input())
+
+  if len(background_color) == 0 and len(background_img) == 0:
+    background_img = 'backgrounds_images/белое_полотно_фотостудии.png' # путь до файла с новым фоном
+    background_color = СOLOR_RGB['thistle'] # цвет для фона
   
-  input_img = '/Users/alyonapashnina/computer_vision_case_photo/data/sirius_data/11.jpg'
+  input_img = 'data/sirius_data/11.jpg' # путь до файла с входной картинкой
+
+  print(background_color, background_img, input_img)
   
-  draw_new_back(input_img, background_color=background_color, background_img_filename=None)
+#   draw_new_back(input_img, background_color=background_color, background_img_filename=None)
